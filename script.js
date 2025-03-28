065adc8da0f6ac4c34aea598be17f699fd3dec0b
Irfan Ullah Afridi <161113197+Irfanullahafridi@users.noreply.github.com>
2025-03-28 01:06:00 -0700

    solved some problems

---
+++ script.js
@@ -0,0 +1,64 @@
+// let age = prompt(`Enter your age`)
+// let num = Number(name)
+// console.log(`${name} data type is ${typeof name} ${num*2} data type is ${typeof num}`)
+
+
+// Conditional statement
+// Check even or odd
+// let n = prompt("Enter a number")
+//  num = Number(n)
+// if (num % 2 === 0){
+//     console.log("You enter even number which is", num, "and type is", typeof(num))
+// } else {
+//     console.log("You enter odd number which is", num, "and type is", typeof(num))
+// }
+
+// Find largest number
+// let n1 = prompt("Enter first number")
+// let n2 = prompt("Enter second number")
+// let n3 = prompt("Enter third number",)
+
+// num1 = Number(n1);
+// num2 = Number(n2);
+// num3 = Number(n3);
+
+// console.log("You Enter", num1);
+// console.log("You Enter", num2);
+// console.log("You Enter", num3);
+
+// if(num1 > num2 && num1 > num3){
+//     console.log("First num", num1," is largest number and type is", typeof(num1) );
+// } else if(num2 > num1 && num2 > num3){
+//     console.log(num2, "is largest number and type is", typeof(num2));
+    
+// } else{
+//     console.log(num3, "is largest number and type is", typeof(num3) );
+// }
+
+// Check postive negative and zero number
+// let n = prompt("Enter a number")
+// num = Number(n);
+// if (num > 0) {
+//     console.log(`You enter positive number ${num}`)
+// } else if(num < 0) {
+//     console.log(`You enter negative number ${num}`);
+// } else{
+//     console.log("You enter zero number", num); 
+// }
+
+// Simple Grading System
+// let ob = prompt("Enter your obtain Marks")
+
+// if (ob >= 90 && ob <= 100) {
+//     console.log("You obtain A grade");
+// } else if (ob >= 80 && ob < 90) {
+//     console.log("You obtain B grade");
+// } else if (ob >=70 && ob < 80) {
+//     console.log("You obtain C grade");
+// } if (ob >= 60 && ob < 70) {
+//     console.log("You obtain D grade");
+// } else if(ob < 60 && ob > 0){
+//     console.log("You obtain F grade");
+// } else {
+//     console.log("Invalid marks please Enter marks between 0 to 100")
+// }
